e724a31050f9c15021c3db8184511693b31d6156
khushi667 <khushi.mis03@gmail.com>
2024-09-02 00:12:17 +0530

    changes on 2/9/24

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes.js"
+import authRoutes from "./routes/authRoutes.js";
 import connectToMongoDb from "./db/connectToMongoDb.js";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-dotenv.config();
+app.use(express.json()); // Middleware to parse JSON
 
-app.use(express.json());
-
-app.use("/api/auth",authRoutes);
-
-// app.get("/", (req,res) => {
-//     res.send("hello world!!");
-// })
+app.use("/api/auth", authRoutes); // Routes mounted at /api/auth
 
 app.listen(PORT, () => {
-    connectToMongoDb();
-    console.log(`Server is Running on port ${PORT}`)
+  connectToMongoDb();
+  console.log(`Server is Running on port ${PORT}`);
 });
